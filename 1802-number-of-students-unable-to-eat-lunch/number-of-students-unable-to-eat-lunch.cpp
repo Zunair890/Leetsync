91fc392de4c99305f14b8edbb6c87aa05cf3a42f
Zunair Ali <154310361+Zunair890@users.noreply.github.com>
2024-08-27 14:37:36 +0500

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.30%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int count1 = 0, count0 = 0;
-        
-        // Count the number of students preferring each type of sandwich
-        for (int x : students) {
-            if (x)
-                count1++;
-            else
-                count0++;
+        int zeroCount=0;
+        int oneCount=0;
+        for(auto c : students){
+            if(c==0) zeroCount++;
+            else  oneCount++;
         }
-
-        // Iterate through the sandwiches
-        for (int x : sandwiches) {
-            // If the sandwich on top is preferred by a student
-            if (x) {
-                // If there are still students who prefer this type of sandwich
-                if (count1 > 0) {
-                    count1--; // Reduce the count of students preferring this type of sandwich
-                } else {
-                    // If no students prefer this type of sandwich, return the count of students unable to eat
-                    return count0;
-                }
-            } else {
-                // If the sandwich on top is not preferred by a student
-                // If there are still students who prefer this type of sandwich
-                if (count0 > 0) {
-                    count0--; // Reduce the count of students preferring this type of sandwich
-                } else {
-                    // If no students prefer this type of sandwich, return the count of students unable to eat
-                    return count1;
-                }
+        for(auto d : sandwiches){
+            if(d==0 && zeroCount==0){
+                return oneCount;
+            }
+            else if(d==1 && oneCount==0){
+                return zeroCount;
+            }
+            else if(d==0) zeroCount--;        
+               else{
+                oneCount--;
             }
         }
-        return 0; // If all students can eat, return 0
+        return 0;
+
     }
 };
